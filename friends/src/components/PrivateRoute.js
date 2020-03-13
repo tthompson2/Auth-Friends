dcff21ca16480c6d2a8f8622be2d89af6c62bf0c
Trevor Thompson <trevthom18@gmail.com>
2020-03-13 19:32:03 -0400

    Pushing the yarn installation as well as the axios replacment utility function and the PrivateRoute setup component

---
+++ friends/src/components/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import { Route, Redirect} from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest}) => {
+    return (
+        <Route 
+        
+        {...rest}
+
+        render={props => {
+
+            if(localStorage.getItem("token")) {
+                return <Component {...props} />;
+            }  else {
+                return <Redirect to="/login"/>;
+            }
+        }}
+
+        />
+    )
+};
+
+export default PrivateRoute;
+
+// this code is used to create a boiler plate private wrapper that will ensure the code you return to the user will only 
+// allow them to access certain pages once it is confirmed on the client machine that a token has been stored for the user 
+// attempting to access the page
